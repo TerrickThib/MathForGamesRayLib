2669ce74ee1e465b72b75fe0d70f629354b29008
Terrick Thibodeaux <74807517+TerrickThib@users.noreply.github.com>
2021-10-18 15:23:54 -0500

    Initialize Raylib

---
+++ MathForGames/Actor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    struct Icon
+    {
+        public char Symbol;
+        public Color Color;
+    }
+    class Actor
+    {
+        private Icon _icon;
+        private string _name;
+        private Vector2 _position;
+        private bool _started;
+
+        /// <summary>
+        /// True if the start function has been called for this actor
+        /// </summary>
+        public bool Started
+        {
+            get { return _started; }
+        }
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }  
+        
+        public Icon icon
+        {
+            get { return _icon; }
+        }
+
+        public Actor(char icon, float x, float y, Color color, string name = "Actor")
+            : this(icon, new Vector2 { X = x, Y = y}, color, name) {}
+        
+        public Actor(char icon, Vector2 position, Color color, string name = "Actor")
+        {
+            _icon = new Icon { Symbol = icon, Color = color };
+            _position = position;
+            _name = name;
+        }
+
+        public virtual void Start()
+        {
+            _started = true;
+        }
+
+        public virtual void Update(float deltaTime)
+        {
+            Console.WriteLine(_name + ": " + Position.X + ", " + Position.Y);
+        }
+
+        public virtual void Draw()
+        {
+            Raylib.DrawText(icon.Symbol.ToString(), (int)Position.X, (int)Position.Y, 50, icon.Color);
+        }  
+
+        public void End()
+        {
+
+        }
+
+        public virtual void OnCollision(Actor actor)
+        {
+
+        }
+    }
+}
---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public struct Vector2
+    {
+        public float X;
+        public float Y;
+
+
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// GEts the length of the vector
+        /// </summary>
+        public float Magnitude
+        {
+            get
+            {
+                //The X squared plus Y squared
+                return (float)Math.Sqrt(X * X + Y * Y);
+            }
+        }
+
+        /// <summary>
+        /// Changes this vector to have a magnitude that is equal to one
+        /// </summary>
+        /// <returns>The result of the normalization. Returns a empty vector if the magnitude is zero</returns>
+        public Vector2 Normalize()
+        {
+            if (Magnitude == 0)
+                return new Vector2();
+
+            return this / Magnitude;
+        }
+
+        /// <summary>
+        /// Gets the normalized version of this vector without changing it
+        /// </summary>
+        public Vector2 Normalized
+        {
+            get
+            {
+                Vector2 value = this;
+                return value.Normalize();
+            }
+        }
+        /// <summary>
+        /// Adds the x value of the second vector to the first, and addds the y value of the second vector to the first
+        /// </summary>
+        /// <param name="lhs">The vector that is increasing</param>
+        /// <param name="rhs">The vector used to increase the 1st vector</param>
+        /// <returns>The result of the vector addition</returns>
+        public static Vector2 operator + (Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2 { X = lhs.X + rhs.X, Y = lhs.Y + rhs.Y };            
+        }
+
+        /// <summary>
+        /// Subtracts the x value of the second vector to the first, and subtracts the y value of the second vector to the first
+        /// </summary>
+        /// <param name="lhs">The vector that is being subtracted from</param>
+        /// <param name="rhs">The vector used to subtract from  the 1st vector</param>
+        /// <returns>The result of the vector addition</returns>
+        public static Vector2 operator - (Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2 { X = lhs.X - rhs.X, Y = lhs.Y - rhs.Y };
+        }
+
+        /// <summary>
+        /// Multiplies the vectors x and y values by the scaler
+        /// </summary>
+        /// <param name="lhs">The vector that is being scaled</param>
+        /// <param name="scalar">The value to scale the vector by</param>
+        /// <returns>The result of the vector scaling</returns>
+        public static Vector2 operator *(Vector2 lhs, float scalar)
+        {
+            return new Vector2 { X = lhs.X * scalar, Y = lhs.Y * scalar };
+        }
+
+        /// <summary>
+        /// Divides the vector's x and y values by the scalar given
+        /// </summary>
+        /// <param name="lhs">The vector that is being scaled</param>
+        /// <param name="scalar">The value to scale the vector by</param>
+        /// <returns>The result of the vector scaling</returns>
+        public static Vector2 operator /(Vector2 lhs, float scalar)
+        {
+            return new Vector2 { X = lhs.X / scalar, Y = lhs.Y / scalar };
+        }
+        
+       /// <summary>
+       /// Compares the x and y values of two vectors
+       /// </summary>
+       /// <param name="lhs">The left side of the comparison</param>
+       /// <param name="rhs">The right side of the comparison</param>
+       /// <returns>True if the x values of both vectors match and the y values match</returns>
+        public static bool operator ==(Vector2 lhs, Vector2 rhs)
+        {
+            return lhs.X == rhs.X && lhs.Y == rhs.Y;            
+        }
+
+        /// <summary>
+        /// Compares the x and y values of two vectors
+        /// </summary>
+        /// <param name="lhs">The left side of the comparison</param>
+        /// <param name="rhs">The right side of the comparison</param>
+        /// <returns>True if the values of X isnt equaled to the values of Y</returns>
+        public static bool operator !=(Vector2 lhs, Vector2 rhs)
+        {
+            return lhs.X != rhs.X && lhs.Y != rhs.Y;           
+        }
+
+    }
+}
